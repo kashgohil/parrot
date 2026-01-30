cb1ba69bf27fa389f7d8a6958a396195c66ee86b
kash gohil <kashyapgohil476@gmail.com>
2026-01-30 06:48:55 +0530

    added fira fonts, added motion, added fonts to css file to apply to app

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,22 +1,22 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
 import { AuthProvider } from "./lib/auth";
+import { routeTree } from "./routeTree.gen";
 import "./styles.css";
 
 const router = createRouter({ routeTree });
 
 declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
+	interface Register {
+		router: typeof router;
+	}
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<AuthProvider>
+			<RouterProvider router={router} />
+		</AuthProvider>
+	</React.StrictMode>,
 );
